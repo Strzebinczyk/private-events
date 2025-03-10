0ad662eb15f51032654ab059615d35db0b3620af
Strzebinczyk <kar.strzebinczyk@gmail.com>
2025-03-10 11:23:37 +0100

    Fix tests

--- spec/features/user_logs_out_spec.rb
+++ spec/features/user_logs_out_spec.rb
@@ -4,9 +4,11 @@ require 'spec_helper'
 require 'rails_helper'
 
 RSpec.describe 'User signs out', type: :feature do
-  @user1 = create(:user)
-  log_in_with @user1.email, @user1.password
-  click_button 'Sign out'
+  it 'logs the user out' do
+    @user1 = create(:user)
+    log_in_with @user1.email, @user1.password
+    click_link 'Sign out'
 
-  expect(page).to have_content('Signed out successfully.')
+    expect(page).to have_content('Signed out successfully.')
+  end
 end
